877fe5e6e04715e07607075a690adceb116fa0b3
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-12-28 23:59:59 +0100

    refactor xd

--- NutriApp.Server/Controllers/WeatherForecastController.cs
+++ NutriApp.Server/Controllers/WeatherForecastController.cs
@@ -35,4 +35,15 @@ namespace NutriApp.Server.Controllers
                 .ToArray();
         }
     }
+
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
 }
